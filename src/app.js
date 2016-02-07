00beb55b43d4bc5491d1a4d039f03c529ab89b43
Colemalban <colemalban@gmail.com>
2016-02-07 09:25:00 -0500

    ITS LIT

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ var Vector2 = require('vector2');
 Firebase.INTERNAL.forceWebSockets();  
 var wind = new UI.Window();
 //function used to vibrate
-function vibrate(distance,side){
+/*function vibrate(distance,side){
     var length;
     if(side==0){
         length='double';
@@ -37,7 +37,7 @@ function vibrate(distance,side){
             Vibe.vibrate(length);
             break;
     }
-    }
+    }*/
 
 
 var textfield = new UI.Text({
@@ -45,6 +45,13 @@ var textfield = new UI.Text({
     size: new Vector2(144, 30),
     text: "Loading.....",
     textAlign:'left'
+});
+var nameText = new UI.Text({
+    position: new Vector2(0, 50),
+    size: new Vector2(144, 30),
+    text: "CastleHacks",
+    textAlign:'center',
+    textOverflow: 'wrap'
 });
 var textTwo = new UI.Text({
     position: new Vector2(0,50),
@@ -54,22 +61,62 @@ var textTwo = new UI.Text({
 });
 wind.add(textfield);
 wind.add(textTwo);
+//wind.add(nameText);
 wind.show();
-
 var childRef = ref.child('UserData').child('Pebble2961');
 childRef.on('value', function(value){
-    var right = value.val().r;
-    var left = value.val().l;
-    side = (left<right)?'short':'double';
-    dist = (left<right)?left:right;
-    vibrate(dist,side);
-    for(var i=0;i<dist*1000;i++){
-        //ayylmao
-    }
+    
+   var point = value.val();
+   var left=point.l;
+   var right = point.r;
+   var sum = left+right;
+    var average = sum/2;
+    var distance;
+    
     console.log("Left:"+left);
     console.log("Right:"+right);
     textfield.text("Left:"+left);
     textTwo.text("Right:"+right);
+    /*
+    if(average<65){
+        if (left<right) {
+            Vibe.vibrate('short');
+            distance = left;
+        } else {
+            Vibe.vibrate('double');
+            distance = right;
+        }
+       
+        if(distance<65){
+            for(var i=(distance*distance*distance*distance*5);i>0;i--){
+            
+        }
+        }
+    }*/
+    
+    if(sum/2<=25){
+        Vibe.vibrate('long'); 
+        for(var i=0;i<1000000;i+=4){
+            //
+        }
+    }
+    if(sum/2<=45){
+        Vibe.vibrate('double');
+        for(var i=0;i<10000000;i+=2){
+            //
+        }
+        
+    }
+    if(sum/2<=65){
+        Vibe.vibrate('short');
+        for(var i=0;i<100000000;i+=2){
+            //
+        }
+    }
+    
+    
+    
+    childRef.remove();
 }
 );
 
